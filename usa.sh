b1372c6fc21fe6a667d450789b36f9bfb03c4da8
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-08-14 19:18:44 +0300

    added tunneling script

---
+++ usa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xterm -e "ssh -D 1080 hosting" &
+sudo sh -c "/bin/cp /home/mperdikeas/.Xauthority /home/slash"
+sudo sh -c "/bin/chown slash /home/slash/.Xauthority"
+sudo sh -c "/bin/chmod 640 /home/slash/.Xauthority"
+sudo sh -c "/bin/su - slash"
