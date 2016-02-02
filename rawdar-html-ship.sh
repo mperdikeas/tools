cb095b7c9ee1e10c8100abc8a5e56db176436c6f
mperdikeas <mperdikeas@gmail.com>
2016-02-02 12:48:59 +0200

    rawdar html ship tool prints temp dir used

--- rawdar-html-ship.sh
+++ rawdar-html-ship.sh
@@ -3,11 +3,14 @@ set -e
 readonly PASS_FILE=~/esac.neuropublic.pwd
 readonly DEST=172.31.148.22
 dir=`mktemp -d` && cd $dir
+printf "HTML will be exported in temp dir: [%s]\n" $dir
 rm -f rawdar.html && cp ~/esac-rawdar/rawdar.html .
 rm -fr rawdar.org.files/ && cp -Lr ~/esac-rawdar/rawdar.org.files/ . 2>&1 | grep -v cannot\ stat|| : #http://serverfault.com/a/153893
 rm -f rawdar.zip && zip -r rawdar.zip rawdar.html rawdar.org.files/
+printf "HTML exported in temp dir: [%s]\n" $dir
 sshpass -f $PASS_FILE scp rawdar.zip user@$DEST:~/rawdar.war
 sshpass -f $PASS_FILE ssh user@$DEST <<'ENDSSH'
 ./publish-rawdar.sh
 ENDSSH
+
 
