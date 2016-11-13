12811aea0aa249d90d768d572bce8ad669d047fd
mperdikeas <mperdikeas@gmail.com>
2016-11-13 01:00:31 -0500

    consolas.sh script added comments on the procedure

--- consolas.sh
+++ consolas.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-# http://www.rushis.com/2013/03/consolas-font-on-ubuntu/
+# http://www.rushis.com/consolas-font-on-ubuntu/
+# basic procedure is:
+#  1. $ sudo apt-get install font-manager
+#  2. $ sudo apt-get install cabextract
+#  3. $ cd
+#  4. $ consolas.sh   # (i.e. run this script)
+#  5. $ cd temp       # the above command has created a [temp] directory: cd into it.
+#  5. $ sudo mkdir -p /usr/share/fonts/truetype/consolas
+#  6. $ sudo cp CONSOLA*.TTF /usr/share/fonts/truetype/consolas
+#  7. $ sudo fc-cache -fv
+#  8. launch LibreOffice to verify that the new font (consolas) is visible
+#  9. if need be, reboot
+# 10. cd && rm -fr temp
+
 set -e
 set -x
 mkdir temp
