f191ac10c4d894028758abcc3c14efc58aa151c8
mperdikeas <mperdikeas@gmail.com>
2017-12-01 20:00:35 -0500

    the parallel script can now handle both commands or another script

--- parallel.sh
+++ parallel.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
 then
-    echo "usage is $0 <number-of-parallel-threads> <script to run>"
+    echo "usage is $0 <number-of-parallel-threads> <script or bash invocation to run>"
 else
     N=$1
-    SCRIPT=$2
+    shift
+    SCRIPT_OR_COMMAND="$@"
     # for i in {1.."$N"} 
     # for i in $(seq 1 1 $N) # http://unix.stackexchange.com/q/103121/24044
     for ((i = 1 ; i <= $N ; i++ ))
     do
-        "$SCRIPT" > $i.out 2> $i.err &
+        if [ -e "$SCRIPT_OR_COMMAND" ] # it's a file
+        then
+            "$SCRIPT_OR_COMMAND" > $i.out 2> $i.err &
+        else                           # it's a shell command, run it with eval
+            eval "$SCRIPT_OR_COMMAND > $i.out 2> $i.err &"
+        fi
     done
 fi
 
